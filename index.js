e275e2cabfa80fa3e5ffa73d4121fa92eb8a7245
Linah Muhonja <linahmuhonjaimbari@gmail.com>
2022-08-14 21:29:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+function moveDodgerLeft(){    
+    const newNumber = dodger.style.left.replace("px", " ");
+    const left = parseInt(newNumber, 10);
+    dodger.style.left = `${left - 1}px`;
+
+}
+
+function moveDodgerRight(){
+    const newNumber = dodger.style.left.replace("px", " ");
+    const left = parseInt(newNumber, 10);
+    dodger.style.left = `${left + 1}px`;
+}
